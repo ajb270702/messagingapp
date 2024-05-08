d4f7821516f25214e2800b191d94dac90fc868ea
John <akhiljohnboby@gmail.com>
2024-05-08 19:49:41 +1000

    added the spring files to create a rest api

---
+++ BoostMsg/src/main/java/boost/msg/BoostMsg/BoostMsgApplication.java
@@ -0,0 +1,13 @@
+package boost.msg.BoostMsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostMsgApplication.class, args);
+	}
+
+}
---
+++ BoostMsg/src/test/java/boost/msg/BoostMsg/BoostMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package boost.msg.BoostMsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
